c2a706b11e42394e1bf9c658ff9969fde1effb68
Tiago Carbone do Nascimento <carbonetiago@id.uff.br>
2024-01-19 11:17:09 -0300

    Order, Instant, ISO 8601 part 1

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,8 +1,11 @@
 package com.educandoweb.course.entities;
 
 import jakarta.persistence.*;
+import org.aspectj.weaver.ast.Or;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -19,6 +22,9 @@ public class User implements Serializable {
     private String email;
     private String phone ;
     private String password ;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User(){
 
@@ -70,6 +76,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -32,4 +32,6 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
 
+
+
 }
