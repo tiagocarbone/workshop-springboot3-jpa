07a126d232dabc58b7ebdbd40bed11854eecb59f
Tiago Carbone do Nascimento <carbonetiago@id.uff.br>
2024-01-20 15:10:06 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -36,6 +36,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
+
     public Order(){
 
     }
@@ -81,6 +84,14 @@ public class Order implements Serializable {
             this.orderStatus = orderStatus.getCode();
         }
 
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
     }
 
     public Set<OrderItem> getItems(){
